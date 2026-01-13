44a594db45a11f0ab453af16412be9f162ed8094
WSQ.Coder <WSQ.coder@gmail.com>
2026-01-13 23:12:06 +0300

    кнопка "Вернуться" в деталях Product

--- Pages/Product/Details.cshtml.cs
+++ Pages/Product/Details.cshtml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages.Product
 {
@@ -19,13 +14,17 @@ namespace RazorPagesMovie.Pages.Product
         }
 
         public Models.Product Product { get; set; } = default!;
+        public string ReturnUrl { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
-            {
-                return NotFound();
+            {   return NotFound();
             }
+
+            // задаём адрес ссылки для кнопки возврата на предыдыщую страницу
+            // если есть адрес. Или на Индекс
+            ReturnUrl = Request.Headers["Referer"].ToString() ?? "/Index";
 
             var product = await _context.Products
                 .Include(p => p.IdSellerNavigation)
